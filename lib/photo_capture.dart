48078ae8530f6568e6406f91d8919b74be702595
SavviSam <swghazzawi@gmail.com>
2024-06-03 19:38:08 -0600

    camera functionality

--- lib/photo_capture.dart
+++ lib/photo_capture.dart
@@ -1,13 +1,49 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class PhotoCaptureScreen extends StatelessWidget {
-  const PhotoCaptureScreen({
-    super.key,
-    required this.camera,
-  });
+class PhotoCaptureScreen extends StatefulWidget {
+  const PhotoCaptureScreen({super.key, required this.camera});
 
   final CameraDescription camera;
+
+  @override
+  State<PhotoCaptureScreen> createState() => _PhotoCaptureScreenState();
+}
+
+class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.high,
+    );
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _takePicture() async {
+    try {
+      await _initializeControllerFuture;
+      final image = await _controller.takePicture();
+      if (!mounted) return;
+      // You can now use the captured image file.
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Picture taken: ${image.path}')),
+      );
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +53,19 @@ class PhotoCaptureScreen extends StatelessWidget {
         backgroundColor: const Color(0xFFFFF1D0),
         elevation: 0,
       ),
-      body: Center(
-        child: Text(
-          'Camera: ${camera.name}',
-        ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _takePicture,
+        child: const Icon(Icons.camera),
       ),
     );
   }
