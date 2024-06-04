68be819e059cf91ca409fb13d3ec480dabc7a727
SavviSam <swghazzawi@gmail.com>
2024-06-03 20:10:18 -0600

    user take picture and display

--- lib/photo_capture.dart
+++ lib/photo_capture.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PhotoCaptureScreen extends StatefulWidget {
   const PhotoCaptureScreen({super.key, required this.camera});
@@ -31,16 +34,25 @@ class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
   }
 
   Future<void> _takePicture() async {
+    // TODO: update image paths for feeding to DB and AI model
+    // TODO: Allow 2 options after user takes photo - retake picture or generate AI staged photo
     try {
       await _initializeControllerFuture;
       final image = await _controller.takePicture();
+
+      final directory = await getTemporaryDirectory();
+
+      final imagePath = '${directory.path}/${DateTime.now()}.png';
+      await image.saveTo(imagePath);
+
       if (!mounted) return;
       // You can now use the captured image file.
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Picture taken: ${image.path}')),
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => DisplayPictureScreen(imagePath: imagePath)),
       );
     } catch (e) {
-      // ignore: avoid_print
       print(e);
     }
   }
@@ -63,9 +75,31 @@ class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
           }
         },
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: _takePicture,
         child: const Icon(Icons.camera),
+      ),
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Display Picture'),
+        backgroundColor: const Color(0xFFFFF1D0),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Image.file(File(imagePath)),
       ),
     );
   }
